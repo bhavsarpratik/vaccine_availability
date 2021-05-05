fdf2d47fee706884c924fae88b60713cdad3298c
azeem110201 <mohd.abazeem@gmail.com>
2021-05-05 18:32:54 +0530

    commited changes in availability and streamli app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
-
-from availability import get_availability
+from availability import getDetails
+import base64
 
 st.title('Vaccine Availability')
 st.markdown('Contribute on [GitHub](https://github.com/bhavsarpratik/vaccine_availability)')
@@ -10,17 +10,35 @@ st.markdown('Contribute on [GitHub](https://github.com/bhavsarpratik/vaccine_ava
 def get_data():
     return pd.read_csv('districts.csv')
 
+def get_table_download_link(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(
+        csv.encode()
+    ).decode()
+    
+    return f'<a href="data:file/csv;base64,{b64}" download="vaccineData.csv">Download availabile vaccine table in the form of csv</a>'    
+
 data = get_data()
 
-mapper = dict(data.values)
-avail_districts = list(mapper.keys())
-min_age_limit = st.radio('Min age limit', [18, 45])
-option = st.multiselect('District', avail_districts, "Ahmedabad Corporation")
+age_limit = st.radio('Select',["18-44","45+"])
+age_limit_ = int()
+if age_limit == "18-44":
+    age_limit_ = 18
+else:
+    age_limit_ = 45
 
-district_ids = [mapper[val] for val in option]
+district_name = st.selectbox(label='',options=data['district_name'].values)
 
-try:
-    df = get_availability(district_ids, min_age_limit)
-    st.table(df)
-except:
-    st.error('Unable to fetch data. Try after a few minutes')
+district_id = data[data['district_name'] == district_name]['district_id'].values[0]
+
+df = getDetails(district_id, age_limit_)
+
+if isinstance(df, pd.DataFrame):
+	st.markdown(get_table_download_link(df), unsafe_allow_html=True)
+	st.table(df)
+else:
+    st.error(df)         
+
+
+
+
